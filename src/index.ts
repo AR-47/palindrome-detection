acc8aaa08e2db233126cd990208abe732f0a3267
Adil Rahman <arahman.99@outlook.com>
2023-08-07 10:14:02 +0100

    clean up

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,11 @@
 export const isPalindrome = (inputStr: string): boolean => {
   const inputStrAsArr = inputStr.split("");
   if (inputStrAsArr[0] === inputStrAsArr[inputStrAsArr.length - 1]) {
-    if (inputStrAsArr.length > 1) {
+    if (inputStrAsArr.length > 2) {
       const substring = inputStrAsArr.slice(1, -1).join("");
-      console.log(substring);
       return isPalindrome(substring);
     }
     return true;
   }
   return false;
 };
-
-isPalindrome("racecar");
